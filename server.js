df27206bd55b8f9186fdaf5f8af75d66661c087a
Jung Chen <jungwchen@gmail.com>
2016-12-20 17:58:56 -0500

    api test submit

--- server.js
+++ server.js
@@ -13,5 +13,41 @@ app.get('/users', (req, res) => {
   })
 });
 
+app.get('/users/id/:id', (req, res) => {
+	User.findById(req.params.id)
+	.then((data) => {
+		res.send(data);
+	})
+});
+
+app.get('/users/username/:username', (req,res) => {
+	User.findOne({
+		where: {username: req.params.username}
+	})
+	.then((data) => {
+		res.send(data);
+	})
+});
+
+app.get('/users/sort/a-z', (req,res)=>{
+  User.findAll({
+  	order: ['username', 'DESC']
+  })
+  .then((data) => {
+    res.send(data);
+  })
+})
+
+app.post('/users', (req,res) => {
+	User.create({
+		username: req.username,
+		email: req.email,
+		password: req.email
+	})
+	.then((data)=>{
+		res.send(data)
+	})
+})
+
 
 module.exports = app;
--- test/user-tests.js
+++ test/user-tests.js
@@ -9,6 +9,7 @@ describe('User tests', () => {
     {username: 'test1', email: 'test1@gmail.com', password: 'pass1'},
     {username: 'test2', email: 'test2@gmail.com', password: 'pass2'},
     {username: 'test3', email: 'test3@gmail.com', password: 'pass3'},
+    {username: 'abc', email: 'abc@gmail.com', password: 'abc123'},
   ];
   //you can use 'before' to seed your database with data before your tests
   //you only need one 'before' statement
@@ -35,11 +36,66 @@ describe('User tests', () => {
     supertest(server)
       .get('/users')
       .end((err, res) => {
-        expect(res.body.length).equal(3);
+        expect(res.body.length).eql(4);
         expect(res.body[0].username).equal(users[0].username);
         expect(res.body[1].username).equal(users[1].username);
         expect(res.body[2].username).equal(users[2].username);
+        expect(res.body[3].username).equal(users[3].username);
         done();
       })
   });
+
+  //testing the GET individual user by id route
+  it(`'/users/:id' should respond with one user matching that id in an object`, (done) =>{
+    supertest(server)
+      .get('/users/id/1')
+      .end((err,res)=>{
+        expect(typeof res.body).eql('object');
+        expect(res.body.username).equal(users[0].username);
+        expect(res.body.email).equal(users[0].email);
+        expect(res.body.password).equal(users[0].password);
+        done();
+      })
+  });
+
+  //testing the GET individual user by username
+  it(`'/users/:username' should respond with one user matching that username in an array`, (done)=>{
+    supertest(server)
+      .get('/users/username/test2')
+      .end((err,res)=>{
+        expect(res.body.length).eql(undefined);
+        expect(res.body.username).equal(users[1].username);
+        expect(res.body.email).equal(users[1].email);
+        expect(res.body.password).equal(users[1].password);
+        done();
+      })
+  });
+
+  //testing the GET users sorted a-z by username
+  it(`'/users/sort/a-z' should respond with all users with their name sorted alphabetically`, (done)=>{
+    supertest(server)
+      .get('/users/sort/a-z')
+      .end((err,res)=>{
+        expect(res.body.length).equal(4);
+        expect(res.body[0].username).equal(users[4].username);
+        expect(res.body[1].username).equal(users[0].username);
+        expect(res.body[2].username).equal(users[1].username);
+        expect(res.body[3].username).equal(users[2].username);
+        done();
+      })
+  });
+
+  //testing the POST a new user
+  it(`'/users' should respond with information of user that was created`, (done)=>{
+    supertest(server)
+      .post('/users')
+      .send({username: 'test4', email: 'test4@gmail.com', password: 'pass4'})
+      .get('/users/username/test4')
+      .end((err,res)=>{
+        expect(res.body.username).equal('test4');
+        expect(res.body.email).equal('test4@gmail.com');
+        expect(res.body.password).equal('pass4');
+        done();
+      })
+  })
 });
